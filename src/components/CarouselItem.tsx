fa6b48fa05086dd79c90a7cf53d3f6ce8fa5354f
anbyjap <haruru898@gmail.com>
2025-02-06 02:05:17 +0900

    # feat : adopt grid to project more

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NewlineText } from "./NewLineText";
 import { CarouselItemProps } from "../types";
 import { SkillsIcon } from "./SkillsIcon";
@@ -11,69 +11,59 @@ interface NewCarouselItemProps extends CarouselItemProps {
 }
 
 const CarouselItem: React.FC<NewCarouselItemProps> = (props) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => setIsHovered(true);
-  const handleMouseLeave = () => setIsHovered(false);
-
   if (props.hidden) return null;
 
   return (
     <div
       onClick={props.onExpand}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
       className={`
-        bg-white rounded shadow overflow-hidden cursor-pointer transition-transform 
-        duration-300 transform ${isHovered ? "scale-105" : ""}
+        col-span-6 sm:col-span-6 lg:col-span-4 mt-2
+        bg-white rounded overflow-hidden cursor-pointer
+        max-w-sm hover:shadow-card
         ${props.expanded ? "ring-4 ring-blue-300" : ""}
-        mb-4
       `}
     >
-      <img
-        src={props.imgSrc}
-        alt={props.name}
-        className="w-full h-[140px] p-4 pt-0 object-contain"
-      />
-
-      <div className="p-4">
-        <h5 className="text-xl font-semibold mb-2">{props.name}</h5>
-
-        <div className="flex flex-wrap gap-2 mb-2">
-          {props.skills.map((skillName) => (
-            <SkillsIcon key={skillName} name={skillName} />
-          ))}
-        </div>
-
-        {props.expanded && (
-          <div className="mt-4">
-            <div className="flex items-center justify-between pb-2 border-b">
-              <div className="flex flex-wrap gap-2">
-                {props.skills.map((skillName) => (
-                  <SkillsIcon key={skillName} name={skillName} />
-                ))}
-              </div>
-              <span className="text-xl">&darr;</span>
-            </div>
-
-            <div className="mt-2">
-              <NewlineText text={props.description} />
-            </div>
-
-            {props.link !== "" && (
-              <div className="flex justify-end mt-4">
-                <a
-                  href={props.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-blue-500 hover:underline"
-                >
-                  ⇒Go and see it
-                </a>
-              </div>
-            )}
+      <div className="flex h-full flex-col justify-between">
+        <img
+          src={props.imgSrc}
+          alt={props.name}
+          className="w-fit object-contain flex flex-col justify-between flex-grow"
+        />
+        <div className="p-3 min-h-[120px] max-h-[150px]">
+          <h5 className="text-xl font-semibold">{props.name}</h5>
+          <div className="flex flex-wrap gap-2">
+            {props.skills.map((skillName) => (
+              <SkillsIcon key={skillName} name={skillName} />
+            ))}
           </div>
-        )}
+          {props.expanded && (
+            <div className="mt-4">
+              <div className="flex items-center justify-between pb-2 border-b">
+                <div className="flex flex-wrap gap-2">
+                  {props.skills.map((skillName) => (
+                    <SkillsIcon key={skillName} name={skillName} />
+                  ))}
+                </div>
+                <span className="text-xl">&darr;</span>
+              </div>
+              <div className="mt-2">
+                <NewlineText text={props.description} />
+              </div>
+              {props.link !== "" && (
+                <div className="flex justify-end mt-4">
+                  <a
+                    href={props.link}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    ⇒Go and see it
+                  </a>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -145,82 +145,80 @@ export const Home: React.FC = () => {
           </div>
         </div>{" "}
         {/* projects */}
-        <div className="col-span-12">
-          <h1 className="text-3xl text-font font-bold mb-4">Projects</h1>
-
-          <div className="animate-fadeInUp overflow-y-auto mb-8">
-            {projects.map((project, i) => (
-              <CarouselItem
-                key={i}
-                name={project.name}
-                description={project.description}
-                skills={project.skills}
-                imgSrc={project.imgSrc}
-                link={project.link}
-                expanded={i === expandedItemIndex}
-                expandedItemIndex={expandedItemIndex}
-                hidden={expandedItemIndex !== null && i !== expandedItemIndex}
-                onExpand={() => toggleExpand(i)}
-              />
-            ))}
-          </div>
-
-          {expandedItemIndex !== null && (
+        <h1 className="col-span-12 text-3xl text-font font-bold mb-4">
+          Projects
+        </h1>
+        {/* <div className="animate-fadeInUp overflow-y-auto mb-8"> */}
+        {projects.map((project, i) => (
+          <CarouselItem
+            key={i}
+            name={project.name}
+            description={project.description}
+            skills={project.skills}
+            imgSrc={project.imgSrc}
+            link={project.link}
+            expanded={i === expandedItemIndex}
+            expandedItemIndex={expandedItemIndex}
+            hidden={expandedItemIndex !== null && i !== expandedItemIndex}
+            onExpand={() => toggleExpand(i)}
+          />
+        ))}
+        {/* </div> */}
+        {expandedItemIndex !== null && (
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+            onClick={() => toggleExpand(null)}
+          >
             <div
-              className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-              onClick={() => toggleExpand(null)}
+              className="relative bg-white p-6 max-w-4xl w-1/2 h-[80%] overflow-y-auto rounded shadow-lg"
+              onClick={(e) => e.stopPropagation()}
             >
-              <div
-                className="relative bg-white p-6 max-w-4xl w-1/2 h-[80%] overflow-y-auto rounded shadow-lg"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <div>
-                  {projects[expandedItemIndex]?.projectScreenshots?.map(
-                    (screenshot, index) => (
-                      <div key={index}>
-                        <img
-                          className="w-full h-auto object-contain"
-                          src={screenshot}
-                          alt={`Screenshot ${index}`}
-                        />
-                      </div>
-                    ),
-                  )}
-                </div>
-
-                <div className="bg-white mt-4">
-                  <h2 className="text-2xl font-bold">
-                    {projects[expandedItemIndex].name}
-                  </h2>
-                  <div>
-                    <h2 className="flex space-x-2">
-                      {projects[expandedItemIndex].skills.map((skillName) => (
-                        <SkillsIcon key={skillName} name={skillName} />
-                      ))}
-                    </h2>
-                    <p className="mt-2">
-                      <NewlineText
-                        text={projects[expandedItemIndex].description}
+              <div>
+                {projects[expandedItemIndex]?.projectScreenshots?.map(
+                  (screenshot, index) => (
+                    <div key={index}>
+                      <img
+                        className="w-full h-auto object-contain"
+                        src={screenshot}
+                        alt={`Screenshot ${index}`}
                       />
-                    </p>
-                  </div>
-                  {projects[expandedItemIndex].link !== "" && (
-                    <div className="flex justify-end mt-4">
-                      <a
-                        href={projects[expandedItemIndex].link}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="text-blue-500 hover:underline"
-                      >
-                        ⇒Go and see it
-                      </a>
                     </div>
-                  )}
+                  ),
+                )}
+              </div>
+
+              <div className="bg-white mt-4">
+                <h2 className="text-2xl font-bold">
+                  {projects[expandedItemIndex].name}
+                </h2>
+                <div>
+                  <h2 className="flex space-x-2">
+                    {projects[expandedItemIndex].skills.map((skillName) => (
+                      <SkillsIcon key={skillName} name={skillName} />
+                    ))}
+                  </h2>
+                  <p className="mt-2">
+                    <NewlineText
+                      text={projects[expandedItemIndex].description}
+                    />
+                  </p>
                 </div>
+                {projects[expandedItemIndex].link !== "" && (
+                  <div className="flex justify-end mt-4">
+                    <a
+                      href={projects[expandedItemIndex].link}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-blue-500 hover:underline"
+                    >
+                      ⇒Go and see it
+                    </a>
+                  </div>
+                )}
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </main>
     </div>
   );
