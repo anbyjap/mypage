e7ffe16db406902b2bde537318895585927ce4b5
anbyjap <haruru898@gmail.com>
2025-02-14 23:02:25 +0900

    # add : resume

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import React, { useRef, useState } from "react";
 import { projects } from "../data/projects";
 import CarouselItem from "../components/CarouselItem";
 import { FaGithub, FaLinkedin, FaMailBulk, FaCopy } from "react-icons/fa";
+import { MdFileDownload } from "react-icons/md";
 import { ProjectModal } from "../components/ProjectModal";
 import toast, { Toaster } from 'react-hot-toast';
+import resumeFile from '../../public/Resume.pdf';
 
 function scrollToElement(
   element: HTMLHeadingElement | null,
@@ -100,16 +102,17 @@ export const Home: React.FC = () => {
                     </a>
                   </button>
                 </div>
-                <a
-                  href="https://murphycharity.org/"
-                  target="_blank"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-font bg-main rounded-lg hover:shadow-lg"
+                <button
+                  onClick={() => {
+
+                  }}
+                  className="px-4 py-2 text-sm font-medium text-font bg-main rounded-lg hover:shadow-lg"
                 >
-                  See Resume
-                  <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                  </svg>
-                </a>
+                  <a className="flex items-center justify-around " href={resumeFile} download="haruki_ambai_resume">
+                    <MdFileDownload size={15} className="mr-1" />
+                    Download Resume
+                  </a>
+                </button>
               </div>
 
             </div>
